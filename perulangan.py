a060b32b56aea2eb9a6603b2e41b507305c14651
Ming Fee <118496518+MingPunyaGit@users.noreply.github.com>
2023-03-07 12:15:56 +0700

    Add files via upload

---
+++ perulangan.py
@@ -0,0 +1,41 @@
+# for i in range(5, 100, 5):
+#     print(i)
+
+# for i in range(1, 51):
+#     if i ==20:
+#         break
+# print(i)
+
+# def pentagon(n):
+#     for i in range(1, n+1):
+#         bilangan = int(i * (3*i-1) / 2)
+#         print(bilangan, end="\t")
+
+# print("Deret n bilangan pentagon: ")
+# n = int(input("Masukan n: "))
+# pentagon(n)
+
+# def cek_prima(n):
+#     pembagi = 0
+#     for i in range(1, n+1):
+#         if n % i == 0:
+#             pembagi = pembagi + 1
+#     if pembagi == 2:
+#         return True
+#     else :
+#             return False
+
+# print("Program pengecekan prima")
+# n = int(input("Masukan bilangan: "))
+
+# hasil = cek_prima(n) 
+
+# if hasil == True:
+#     print(f"{n} adalah bilangan prima ")
+# else:
+#     print(f"{n} Bukan bilangan prima")
+
+# import prima 
+
+# batas = int(input("Masukan batas : "))
+# for i in range 
